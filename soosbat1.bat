210c4094dee9da3df7896a4e36158ae487dfaf1b
PEIJIN CHEN <PCHEN@SOOS.IO>
2021-09-08 18:23:57 -0400

    added the bat file

---
+++ soosbat1.bat
@@ -0,0 +1,28 @@
+
+:: run soos.py with the -h flag for help
+:: ARGS REQUIRING CUSTOMIZATION:
+set "SOOS_PROJECT_NAME=bat_man_testing"
+
+:: ARGS WHERE CUSTOMIZATION IS OPTIONAL:
+set "SOOS_MODE=run_and_wait"
+set "SOOS_ON_FAILURE=fail_the_build"
+set "SOOS_DIRS_TO_EXCLUDE=soos"
+set "SOOS_FILES_TO_EXCLUDE="
+set "SOOS_ANALYSIS_RESULT_MAX_WAIT=300"
+set "SOOS_ANALYSIS_RESULT_POLLING_INTERVAL=10"
+set "SOOS_CHECKOUT_DIR=../"
+
+:: ARGS WHERE CUSTOMIZATION IS OPTIONAL, BUT UNLIKELY:
+set "SOOS_API_BASE_URL=https://dev-api.soos.io/api/"
+
+set "ROOT=%CD%/soos" 
+set "WORKSPACE=%ROOT%/workspace" 
+::#mkdir "%WORKSPACE%"
+cd %ROOT%
+python -m venv ./
+
+cd %WORKSPACE%
+pip3 install -r requirements.txt
+
+python soos.py -m="%SOOS_MODE%" -of="%SOOS_ON_FAILURE%" -dte="%SOOS_DIRS_TO_EXCLUDE%" -fte="%SOOS_FILES_TO_EXCLUDE%" -wd="%SOOS_CHECKOUT_DIR%" -armw=%SOOS_ANALYSIS_RESULT_MAX_WAIT% -arpi=%SOOS_ANALYSIS_RESULT_POLLING_INTERVAL% -buri="%SOOS_API_BASE_URL%" -scp="%SOOS_CHECKOUT_DIR%" -pn="%SOOS_PROJECT_NAME%" -ch="%SOOS_COMMIT_HASH%" -bn="%SOOS_BRANCH_NAME%" -bruri="%SOOS_BRANCH_URI%" -bldver="%SOOS_BUILD_VERSION%" -blduri="%SOOS_BUILD_URI%" -oe="%SOOS_OPERATING_ENVIRONMENT%" -intn="%SOOS_INTEGRATION_NAME%"
+
